50b6fa917a05e5bb97c6cda31764f5462654d06e
Anna <a.baranova@strategiedigitali.io>
2023-07-27 12:00:34 +0200

    Updated links

--- viewerPage/rollup.config.js
+++ viewerPage/rollup.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
    import resolve from '@rollup/plugin-node-resolve';
    import commonjs from '@rollup/plugin-commonjs'
 
@@ -14,21 +13,4 @@ export default {
     resolve(),
     commonjs()
   ]
-=======
-   import resolve from '@rollup/plugin-node-resolve';
-   import commonjs from '@rollup/plugin-commonjs'
-
-export default {
-  input: 'viewer.js',
-  output: [
-    {
-      format: 'esm',
-      file: 'bundleviewer.js'
-    },
-  ],
-  plugins: [
-    resolve(),
-    commonjs()
-  ]
->>>>>>> d612ec9654c39a63099b794dc1f9b82cf723eba0
-};
+}
--- viewerPage/viewer.js
+++ viewerPage/viewer.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Drawing from 'dxf-writer';
 import { Color, LineBasicMaterial, MeshBasicMaterial } from 'three';
 import { IfcViewerAPI } from 'web-ifc-viewer';
@@ -52,58 +51,3 @@ async function loadIfc(url) {
   }
 
 loadIfc('./Eames_PlasticArmchair_DAX.ifc'); 
-=======
-import Drawing from 'dxf-writer';
-import { Color, LineBasicMaterial, MeshBasicMaterial } from 'three';
-import { IfcViewerAPI } from 'web-ifc-viewer';
-
-const container = document.getElementById('viewer-container');
-const viewer = new IfcViewerAPI({ container, backgroundColor: new Color(0xffffff) });
-
-// Create grid and axes
-//viewer.grid.setGrid();
-//viewer.axes.setAxes();
-
-let allPlans;
-let model;
-
-async function loadIfc(url) {
-		// Load the model
-    model = await viewer.IFC.loadIfcUrl(url);
-    
-
-    // Setup camera controls
-    const controls = viewer.context.ifcCamera.cameraControls;
-    controls.setPosition(1, 2, 1, false);
-    controls.setTarget(0, 0.4, 0, false);
-
-
-		// Add dropped shadow and post-processing efect
-    await viewer.shadowDropper.renderShadow(model.modelID);
-
-
-    const lineMaterial = new LineBasicMaterial({ color: 'black' });
-	  const baseMaterial = new MeshBasicMaterial({
-		polygonOffset: true,
-		polygonOffsetFactor: 1, // positive value pushes polygon further away
-		polygonOffsetUnits: 1,
-	});
-	  await viewer.edges.create('example', model.modelID, lineMaterial, baseMaterial);
-
-    //viewer.dimensions.active = true;
-    //viewer.dimensions.previewActive = true;
-
-    //window.ondblclick = () => {
-    //  viewer.dimensions.create();
-    //}
-
-    //window.onkeydown = (event) => {
-    //  if(event.code == 'Delete') {
-    //    viewer.dimensions.delete();
-    //  }
-    //}
-
-  }
-
-loadIfc('./Eames_PlasticArmchair_DAX.ifc'); 
->>>>>>> d612ec9654c39a63099b794dc1f9b82cf723eba0
