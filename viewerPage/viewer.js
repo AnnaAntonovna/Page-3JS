debf7d7e1c3f1901017e5314caa578c2170e1b9e
Anna <a.baranova@strategiedigitali.io>
2023-08-09 17:06:05 +0200

    Update buttons

--- viewerPage/viewer.js
+++ viewerPage/viewer.js
@@ -25,6 +25,7 @@ async function loadIfc(url) {
 
 		// Add dropped shadow and post-processing efect
     await viewer.shadowDropper.renderShadow(model.modelID);
+    await viewer.plans.computeAllPlanViews(model.modelID);
 
 
     const lineMaterial = new LineBasicMaterial({ color: 'black' });
@@ -35,20 +36,41 @@ async function loadIfc(url) {
 	});
 	  await viewer.edges.create('example', model.modelID, lineMaterial, baseMaterial);
 
-    //viewer.dimensions.active = true;
-    //viewer.dimensions.previewActive = true;
+    // Floor plan viewing
 
-    //window.ondblclick = () => {
-    //  viewer.dimensions.create();
-    //}
+allPlans = viewer.plans.getAll(model.modelID);
 
-    //window.onkeydown = (event) => {
-    //  if(event.code == 'Delete') {
-    //    viewer.dimensions.delete();
-    //  }
-    //}
+const buttonContainer = document.getElementById('button-container');
 
-  }
+for (const plan of allPlans) {
+  const currentPlan = viewer.plans.planLists[model.modelID][plan];
+  console.log(currentPlan);
+
+  const button = document.createElement('button');
+  buttonContainer.appendChild(button);
+  button.textContent = currentPlan.name;
+  button.onclick = () => {
+    viewer.plans.goTo(model.modelID, plan);
+    viewer.edges.toggle('example', true);
+    viewer.shadowDropper.deleteShadow(model.modelID);
+    window.ondblclick = () => {
+      viewer.dimensions.create();
+      viewer.dimensions.createInPlane();
+    }
+
+  };
+}
+
+const button = document.createElement('button');
+buttonContainer.appendChild(button);
+button.textContent = 'Exit';
+button.onclick = () => {
+  viewer.plans.exitPlanView();
+  viewer.edges.toggle('example', false);
+  viewer.plans.computeAllPlanViews(model.modelID);
+  viewer.shadowDropper.renderShadow(model.modelID);
+  };
+}
 
 loadIfc('./Eames_PlasticArmchair_DAX.ifc'); 
 
@@ -57,3 +79,29 @@ const targetDiv = document.querySelector('div:not([class]):not([id])');
 
 // Remove the background color
 targetDiv.style.backgroundColor = 'transparent';
+
+viewer.dimensions.active = true;
+viewer.dimensions.previewActive = true;
+
+window.ondblclick = () => {
+  viewer.dimensions.create();
+  viewer.dimensions.createInPlane();
+}
+
+window.oncontextmenu = () => {
+  viewer.dimensions.cancelDrawing();
+}
+
+window.onkeydown = (event) => {
+  if(event.key === 'Escape') {
+    viewer.dimensions.cancelDrawing();
+  }
+}
+
+window.onkeydown = (event) => {
+  if(event.code === 'Delete') {
+      viewer.dimensions.delete();
+  }
+}
+
+
